9194462c99340c26c4573b6f9be36fa69928f5f1
xanoosh <xanoosh@gmail.com>
2024-09-06 10:16:26 +0200

    added persistQueryClient from tanstack query 4 weather data

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,26 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 1, // 1 hour,
+    },
+  },
+});
+
+persistQueryClient({
+  queryClient: queryClient,
+  persister: createSyncStoragePersister({
+    storage: window.localStorage,
+    serialize: (data) => JSON.stringify(data),
+    deserialize: (data) => JSON.parse(data),
+  }),
+  maxAge: 1000 * 60 * 60 * 1, // 1 hour,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
