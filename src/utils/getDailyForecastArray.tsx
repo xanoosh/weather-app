55119c49a5219f4ca07242b3faebb73c6563f5bb
xanoosh <xanoosh@gmail.com>
2024-09-20 15:22:56 +0200

    getdailyforecastarray function tweaks

--- src/utils/getDailyForecastArray.tsx
+++ src/utils/getDailyForecastArray.tsx
@@ -5,9 +5,13 @@ export const getDailyForecastArray = (
 ) => {
   const dailyForcastArray: Array<valuesInterface> = [];
 
-  timelinesArray.forEach((el, i) => {
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+
+  timelinesArray.forEach((el) => {
+    if (dailyForcastArray?.[4]?.values.length === 24) return;
     const hour = Number(el.time.split('T')[1].split(':')[0]);
-    if (i > 0) {
+    if (dailyForcastArray[dailyForcastArray.length - 1]) {
       if (
         el.time.split('T')[0] ===
         dailyForcastArray[dailyForcastArray.length - 1].date.split('T')[0]
@@ -23,15 +27,17 @@ export const getDailyForecastArray = (
         });
       }
     } else {
-      dailyForcastArray.push({
-        date: el.time,
-        values: [
-          {
-            ...el.values,
-            hour,
-          },
-        ],
-      });
+      if (Number(hour) >= currentHour) {
+        dailyForcastArray.push({
+          date: el.time,
+          values: [
+            {
+              ...el.values,
+              hour,
+            },
+          ],
+        });
+      }
     }
   });
   return dailyForcastArray;
