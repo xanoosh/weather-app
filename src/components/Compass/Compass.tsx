bedb1e1fc08d2a9348a9806d1fea73d45c959b26
xanoosh <xanoosh@gmail.com>
2024-09-23 14:18:39 +0200

    compass made smaller

--- src/components/Compass/Compass.tsx
+++ src/components/Compass/Compass.tsx
@@ -5,17 +5,17 @@ export default function Compass({ windDirection }: compassInterface) {
   if (!windDirection) return null;
   return (
     <div>
-      <div className="relative flex justify-center items-center bg-sky-400 rounded-full w-24 h-24">
-        <div className="absolute top-[.3rem] left-[45%] text-white text-xs font-bold">
+      <div className="relative flex justify-center items-center bg-sky-400 rounded-full w-20 h-20">
+        <div className="absolute top-0 left-[46%] text-white text-[.6rem] font-bold">
           N
         </div>
-        <div className="absolute bottom-[.3rem] left-[45%] text-white text-xs font-bold">
+        <div className="absolute bottom-0 left-[47%] text-white text-[.6rem] font-bold">
           S
         </div>
-        <div className="absolute left-[.3rem] top-[43%] text-white text-xs font-bold">
+        <div className="absolute left-1 top-[37%] text-white text-[.6rem] font-bold">
           W
         </div>
-        <div className="absolute right-[.3rem] top-[43%] text-white text-xs font-bold">
+        <div className="absolute right-1 top-[37%] text-white text-[.6rem] font-bold">
           E
         </div>
 
@@ -32,11 +32,10 @@ export default function Compass({ windDirection }: compassInterface) {
               windDirection,
             ],
             transition: {
-              delay: 0.1,
-              duration: 1,
+              delay: 0.05,
+              duration: 0.7,
             },
           }}
-          //   transition={{ delay: 0.5, duration: 0.7 }}
         >
           <div className="w-0 h-0 border-l-[0.375rem] border-l-transparent border-b-[1.5rem] border-b-rose-400 border-r-[0.375rem] border-r-transparent"></div>
           <div className="w-0 h-0 border-l-[0.375rem] border-l-transparent border-t-[1.5rem] border-t-slate-200 border-r-[0.375rem] border-r-transparent"></div>
