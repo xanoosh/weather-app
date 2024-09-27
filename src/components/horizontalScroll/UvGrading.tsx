b104908e876a518ba59e4f5ec3e36bfc3c795af4
xanoosh <xanoosh@gmail.com>
2024-09-27 14:03:39 +0200

    uvgrading arrow & styling

--- src/components/horizontalScroll/UvGrading.tsx
+++ src/components/horizontalScroll/UvGrading.tsx
@@ -8,10 +8,13 @@ export default function UvGrading({ uvObject }: uvGradingInterface) {
     <div className="flex gap-2">
       <p>{name}</p>
       <div className="h-10 w-10 rounded-full bg-gradient-to-r from-green-500 via-yellow-500 to-red-500 relative -mb-4 flex justify-center items-center">
-        <div className={`h-8 w-[8px] relative z-10 ${degrees} -mt-1`}>
-          <div className="w-0 h-0 border-l-[4px] border-l-transparent border-b-[20px] border-b-sky-600 border-r-[4px] border-r-transparent"></div>
+        <div
+          className={`h-10 pt-[10px] w-[px] relative z-10 ${degrees} -mt-1 flex flex-col items-center`}
+        >
+          <div className="w-0 h-0 border-l-[4px] border-l-transparent border-b-[7px] border-b-slate-500 border-r-[4px] border-r-transparent"></div>
+          <div className="w-[3px] h-[7px] bg-slate-500"></div>
         </div>
-        <div className="rounded-full h-8 w-8 bg-white absolute top-1 left-1 right-1 bottom-1"></div>
+        <div className="rounded-full h-[24px] w-[24px] bg-white absolute top-[8px] left-[8px]"></div>
         <div className="absolute bg-white top-[55%] left-0 right-0 bottom-0"></div>
       </div>
     </div>
--- src/utils/getUvIndexHealthConcern.tsx
+++ src/utils/getUvIndexHealthConcern.tsx
@@ -1,10 +1,12 @@
 export const getUvIndexHealthConcern = (code: number) => {
   if (code >= 0 && code <= 2)
-    return { name: 'Low', degrees: 'rotate-[270deg]' };
+    return { name: `${code} (Low)`, degrees: 'rotate-[270deg]' };
   if (code >= 3 && code <= 5)
-    return { name: 'Moderate', degrees: 'rotate-[315deg]' };
-  if (code >= 6 && code <= 7) return { name: 'High', degrees: 'rotate-[0deg]' };
+    return { name: `${code} (Moderate)`, degrees: 'rotate-[315deg]' };
+  if (code >= 6 && code <= 7)
+    return { name: `${code} (High)`, degrees: 'rotate-[0deg]' };
   if (code >= 8 && code <= 10)
-    return { name: 'Very High', degrees: 'rotate-[45deg]' };
-  if (code >= 11) return { name: 'Extreme', degrees: 'rotate-[90deg]' };
+    return { name: `${code} (Very High)`, degrees: 'rotate-[45deg]' };
+  if (code >= 11)
+    return { name: `${code} (Extreme)`, degrees: 'rotate-[90deg]' };
 };
