3932633267c10324e8779a0d9c2e4b72622327aa
xanoosh <xanoosh@gmail.com>
2024-09-29 13:58:51 +0200

    animated level update

--- src/components/horizontalScroll/AnimatedLevel.tsx
+++ src/components/horizontalScroll/AnimatedLevel.tsx
@@ -6,70 +6,80 @@ export default function AnimatedLevel({ name, level }: animatedLevelInterface) {
     <div className="flex gap-2">
       <p>{name}</p>
       <div className="flex gap-[0.1rem] items-end">
-        {level >= 1 ? (
-          <motion.div
-            animate={{
-              height: ['0rem', '0.4rem'],
-              transition: {
-                duration: 0.1,
-                easing: 'easeOut',
-              },
-            }}
-            className="bg-green-400 w-[.3rem] border border-slate-300"
-          ></motion.div>
-        ) : null}
-        {level >= 1 ? (
-          <motion.div
-            animate={{
-              height: ['0rem', '0.55rem'],
-              transition: {
-                delay: 0.1,
-                duration: 0.09,
-                easing: 'easeIn',
-              },
-            }}
-            className="bg-yellow-400 w-[.3rem] border border-slate-300"
-          ></motion.div>
-        ) : null}
-        {level >= 1 ? (
-          <motion.div
-            animate={{
-              height: ['0rem', '0.7rem'],
-              transition: {
-                delay: 0.2,
-                duration: 0.09,
-                easing: 'easeOut',
-              },
-            }}
-            className="bg-orange-400 w-[.3rem] border border-slate-300"
-          ></motion.div>
-        ) : null}
-        {level >= 1 ? (
-          <motion.div
-            animate={{
-              height: ['0rem', '0.85rem'],
-              transition: {
-                delay: 0.3,
-                duration: 0.09,
-                easing: 'easeOut',
-              },
-            }}
-            className="bg-rose-500 w-[.3rem] border border-slate-300"
-          ></motion.div>
-        ) : null}
-        {level >= 1 ? (
-          <motion.div
-            animate={{
-              height: ['0rem', '1rem'],
-              transition: {
-                delay: 0.4,
-                duration: 0.09,
-                easing: 'easeOut',
-              },
-            }}
-            className="bg-rose-600 w-[.3rem] border border-slate-300"
-          ></motion.div>
-        ) : null}
+        <motion.div
+          animate={
+            level >= 1
+              ? {
+                  backgroundColor: ['#fff', '#4ade80'],
+                  transition: {
+                    duration: 0.2,
+                    easing: 'easeOut',
+                  },
+                }
+              : undefined
+          }
+          className="h-[0.4rem] w-[.3rem] border border-slate-400"
+        ></motion.div>
+        <motion.div
+          animate={
+            level >= 2
+              ? {
+                  backgroundColor: ['#fff', '#facc15'],
+                  transition: {
+                    delay: 0.1,
+                    duration: 0.2,
+                    easing: 'easeOut',
+                  },
+                }
+              : undefined
+          }
+          className="h-[0.55rem] w-[.3rem] border border-slate-400"
+        ></motion.div>
+        <motion.div
+          animate={
+            level >= 3
+              ? {
+                  backgroundColor: ['#fff', '#fb923c'],
+                  transition: {
+                    delay: 0.2,
+                    duration: 0.2,
+                    easing: 'easeOut',
+                  },
+                }
+              : undefined
+          }
+          className="h-[0.7rem] w-[.3rem] border border-slate-400"
+        ></motion.div>
+        <motion.div
+          animate={
+            level >= 4
+              ? {
+                  backgroundColor: ['#fff', '#f43f5e'],
+                  transition: {
+                    delay: 0.3,
+                    duration: 0.2,
+                    easing: 'easeOut',
+                  },
+                }
+              : undefined
+          }
+          className="h-[0.85rem] w-[.3rem] border border-slate-400"
+        ></motion.div>
+        <motion.div
+          animate={
+            level >= 5
+              ? {
+                  backgroundColor: ['#fff', '#e11d48'],
+                  transition: {
+                    delay: 0.4,
+                    duration: 0.2,
+                    easing: 'easeOut',
+                  },
+                }
+              : undefined
+          }
+          className="h-4 w-[.3rem] border border-slate-400"
+        ></motion.div>
       </div>
     </div>
   );
