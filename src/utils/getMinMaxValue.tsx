b9669f3924781c4884aa394a41fb78193dc6fa32
xanoosh <xanoosh@gmail.com>
2024-10-04 08:50:21 +0200

    min max values fix

--- src/utils/getMinMaxValue.tsx
+++ src/utils/getMinMaxValue.tsx
@@ -1,9 +1,7 @@
 export const getMinMaxValue = (arr: Array<number>, variant: 'min' | 'max') => {
   switch (variant) {
     case 'min':
-      return arr.sort((a, b) => a - b)[0] - 5 >= 0
-        ? arr.sort((a, b) => a - b)[0] - 5
-        : 0;
+      return arr.sort((a, b) => a - b)[0] - 5;
     case 'max':
       return arr.sort((a, b) => b - a)[0] + 5;
   }
