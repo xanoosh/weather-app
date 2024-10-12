570273a45c17947ebc6a11c0029692f1a60da5b8
xanoosh <xanoosh@gmail.com>
2024-10-12 09:33:00 +0200

    hour forecast element layout rearranged

--- src/components/horizontalScroll/HorizontalScroll.tsx
+++ src/components/horizontalScroll/HorizontalScroll.tsx
@@ -11,7 +11,7 @@ export default function DayForecastHorizontalScroll({
   if (!context) return null;
   const dayNightArray = getDayNightArray(context.dayNightData, dayForecast);
   return (
-    <div className="flex scrollbar scrollbar-thumb-sky-700 scrollbar-track-transparent overflow-x-scroll gap-3 py-4 mb-6 focus:outline-none">
+    <div className="flex scrollbar scrollbar-thumb-sky-700 scrollbar-track-transparent overflow-x-scroll gap-3 pb-4 mb-6 focus:outline-none">
       {dayForecast.values.map((hourForecast, i) => {
         return (
           <HourForecastElement
--- src/components/horizontalScroll/HourForecastElement.tsx
+++ src/components/horizontalScroll/HourForecastElement.tsx
@@ -20,48 +20,50 @@ export default function HourForecastElement({
     "relative after:absolute after:h-3/5 after:top-1/5 after:-right-2 after:content-[''] after:w-0.5 after:bg-white/30 last:after:w-0";
   const weatherName = getWeatherNameFromCode(hourForecast.weatherCode);
   return (
-    <HourForecastDialog
-      hourForecast={hourForecast}
-      weatherName={weatherName}
-      weatherCode={weatherCode}
-    >
-      <button
-        className={`py-2 px-4 mt-6 flex flex-col gap-2 justify-center items-center text-white rounded min-w-28 border-2 border-transparent focus:outline-none focus:bg-white/20 relative ${afterElementStyle}`}
-      >
-        <div className="absolute flex gap-2 -top-7 left-1 w-full">
-          {hourForecast.hour === sunrise ? (
-            <Badge icon={<SunIcon />} tooltip={`sunrise at ${sunriseString}`} />
-          ) : null}
-          {hourForecast.hour === sunset ? (
-            <Badge icon={<MoonIcon />} tooltip={`sunset at ${sunsetString}`} />
-          ) : null}
-          {hourForecast.precipitationProbability &&
-          hourForecast.precipitationProbability > 0 ? (
-            <Badge
-              icon={<OpacityIcon />}
-              tooltip={`Precipitation probability ~${hourForecast.precipitationProbability}%`}
-            />
-          ) : null}
-        </div>
-        <div className="flex gap-0.5 items-center justify-center">
-          <p className="text-xs font-semibold text-white">
-            {hourForecast.hour}:00
-          </p>
-        </div>
-        <div className="flex justify-center">
-          <HourForecastIcon
-            weatherName={weatherName}
-            weatherCode={weatherCode}
+    <div className="pt-7 relative text-white">
+      <div className="absolute flex gap-2 top-1 left-1 w-full">
+        {hourForecast.hour === sunrise ? (
+          <Badge icon={<SunIcon />} tooltip={`sunrise at ${sunriseString}`} />
+        ) : null}
+        {hourForecast.hour === sunset ? (
+          <Badge icon={<MoonIcon />} tooltip={`sunset at ${sunsetString}`} />
+        ) : null}
+        {hourForecast.precipitationProbability &&
+        hourForecast.precipitationProbability > 0 ? (
+          <Badge
+            icon={<OpacityIcon />}
+            tooltip={`Precipitation probability ~${hourForecast.precipitationProbability}%`}
           />
-        </div>
-        <div>
-          {hourForecast.temperature ? (
-            <p className="text-center text-md font-semibold">
-              {Math.round(hourForecast.temperature)}°C
+        ) : null}
+      </div>
+      <HourForecastDialog
+        hourForecast={hourForecast}
+        weatherName={weatherName}
+        weatherCode={weatherCode}
+      >
+        <button
+          className={`py-2 px-4 flex flex-col gap-2 justify-center items-center rounded min-w-28 border-2 border-transparent focus:outline-none focus:bg-white/20 relative ${afterElementStyle}`}
+        >
+          <div className="flex gap-0.5 items-center justify-center">
+            <p className="text-xs font-semibold text-white">
+              {hourForecast.hour}:00
             </p>
-          ) : null}
-        </div>
-      </button>
-    </HourForecastDialog>
+          </div>
+          <div className="flex justify-center">
+            <HourForecastIcon
+              weatherName={weatherName}
+              weatherCode={weatherCode}
+            />
+          </div>
+          <div>
+            {hourForecast.temperature ? (
+              <p className="text-center text-md font-semibold">
+                {Math.round(hourForecast.temperature)}°C
+              </p>
+            ) : null}
+          </div>
+        </button>
+      </HourForecastDialog>
+    </div>
   );
 }
