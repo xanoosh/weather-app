e054b7de658c8e67901919c9c6320e762f5567ec
xanoosh <xanoosh@gmail.com>
2025-05-14 21:56:44 +0200

    added gradient to animated forecast tab pill

--- src/components/ForecastTabs.tsx
+++ src/components/ForecastTabs.tsx
@@ -21,7 +21,7 @@ export default function ForecastTabs({
       <TabList className="flex border-b-[0.2rem] border-transparent md:justify-center justify-between relative">
         <motion.div
           layoutId="bubble-days"
-          className="bg-blue-400/40 w-1/5  absolute top-0.5 bottom-0.5 rounded-[3rem] z-10"
+          className="bg-gradient-to-r from-[#576eb9] to-blue-800  w-1/5  absolute top-0.5 bottom-0.5 rounded-[3rem] z-10"
           animate={{ left: `${forecastTabPosition * 20}%` }}
           transition={{
             type: 'spring',
